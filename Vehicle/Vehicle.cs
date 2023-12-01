43be2d00836297938bce54cba2474370c5cd1867
OizumiO <152605807+OizumiO@users.noreply.github.com>
2023-12-01 12:51:11 +0100

    work

--- Vehicle/Vehicle.cs
+++ Vehicle/Vehicle.cs
@@ -1,39 +1,58 @@
 
+using System.Linq.Expressions;
+
 public class Car
 {
-  public double velocity { get; set; }
-  public double maxVelocity { get; set; }
-  public int numberOfPassengers { get; set; }
-  public double load { get; set; }
-  public double maxLoad { get; set; }
-  public int numberOfAxles { get; set; }
+  private String name { get; set; }
+  private double velocity { get; set; }
+  private double maxVelocity { get; set; }
+  private int numberOfPassengers { get; set; }
+  private double load { get; set; }
+  private double maxLoad { get; set; }
+  private int numberOfAxles { get; set; }
 
-  public Car(double maxVelocity, double maxLoad)
+  public Car(String name, double maxVelocity, double maxLoad)
   {
+    Console.WriteLine(name + ": creating a new car");
+    this.name = name;
     this.maxVelocity = maxVelocity;
     this.maxLoad = maxLoad;
   }
   public void Drive()
   {
+    Console.WriteLine(name + ": driving car");
     velocity = maxVelocity;
   }
 
   public void Stop()
   {
+    Console.WriteLine(name + ": stopping car");
     velocity = 0;
   }
 
   public void Load(double load)
   {
+    Console.WriteLine(name + ": loading " + load + "kg of cargo");
     if (this.load + load > maxLoad)
-      throw new Exception("Overload by " + (this.load - maxLoad));
+      throw new Exception(name + ": Overload by " + (this.load - maxLoad));
     this.load += load;
   }
 
   public void Unload(double load)
   {
+    Console.WriteLine(this.name + ": unloading " + load + " kg of cargo");
     if (this.load < load)
-      throw new Exception("Not enough load. Only has " + this.load);
+      throw new Exception(this.name + ": Not enough load. Only has " + this.load);
+    if(this.velocity>0)
+      throw new Exception(this.name + ": Cannot unload. Car is driving " + this.velocity);
     this.load -= load;
   }
+  public int sum(int a, int b) {
+    return a + b;
+  }
+
+  public void honk(){
+    Console.WriteLine(name + ": honk");
+  }
 }
+
